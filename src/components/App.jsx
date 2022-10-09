864c71529dd0f8394287a51fc3b5666523d7105d
whiteroma <roma.white95@gmail.com>
2022-10-10 00:21:51 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import Form from './Form/Form';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+  };
+
+  formHandler = data => {
+    console.log(data)
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Phonebook</h1>
+          
+          <Form onSubmit={this.formHandler}/>
+
+          <h2>Contacts</h2>
+          {/* <Filter/>
+    <ContactList/> */}
+        </div>
+      </>
+    );
+  }
+}
